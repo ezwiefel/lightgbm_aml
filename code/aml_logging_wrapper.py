809b3f1fcf18437d3eba13585be9966538d2bfc9
Erik Zwiefel <erik.zwiefel@microsoft.com>
2020-04-09 14:17:01 +0000

    Adds support for arbitrary CLI arguments

---
+++ code/aml_logging_wrapper.py
@@ -0,0 +1,79 @@
+import logging
+import threading
+import os
+import subprocess
+import re
+import sys
+
+MPI_RANK = int(os.environ.get('OMPI_COMM_WORLD_RANK', 0))
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+class AmlLogPipe(threading.Thread):
+
+    def __init__(self, level, run_context):
+        """Setup the object with a logger and a loglevel
+        and start the thread
+        """
+        threading.Thread.__init__(self)
+        self.run_context = run_context
+        self.daemon = False
+        self.level = level
+        self.fdRead, self.fdWrite = os.pipe()
+        self.pipeReader = os.fdopen(self.fdRead)
+        self.start()
+
+    def fileno(self):
+        """Return the write file descriptor of the pipe
+        """
+        return self.fdWrite
+
+    def process_line(self, line):
+        """Parses a single line of output and logs the result to Azure Machine Learning"""
+
+        RESULT_MATCH_STRING = r'Iteration:(?P<iteration>\d+)\,\s(?P<train_test>training|valid_1)\s(?P<metric>\w+)@?(\d?)\s:\s(?P<value>[+-]?\d+\.\d+)'
+        ITERATION_MATCH_STRING = r'(\d+\.\d+)\sseconds\selapsed\,\sfinished\siteration\s(\d+)'
+        DATALOAD_MATCH_STRING = r'Finished\sloading\sdata\sin\s(\d+\.\d+)\sseconds'
+
+        result_match = re.search(RESULT_MATCH_STRING, line, re.I)
+        if result_match and MPI_RANK == 0:
+            r = result_match.groupdict()
+            r['train_test'] = "validation" if r['train_test'] == 'valid_1' else r['train_test']
+            self.run_context.log(
+                f"{r['train_test']}_{r['metric']}", float(r["value"]))
+
+        # iteration_match = re.search(ITERATION_MATCH_STRING, line, re.I)
+        # if iteration_match and MPI_RANK == 0:
+        #     self.run_context.log_row(name="Iteration Time", Rank=int(MPI_RANK), Iteration=int(iteration_match.group(2)),
+        #                        ElapsedSec=float(iteration_match.group(1)))
+
+        dataload_match = re.search(DATALOAD_MATCH_STRING, line, re.I)
+        if dataload_match and MPI_RANK == 0:
+            self.run_context.log(
+                "Data Load Time", float(dataload_match.group(1)))
+
+    def run(self):
+        """Run the thread, logging everything.
+        """
+        for line in iter(self.pipeReader.readline, ''):
+            self.process_line(line)
+            logging.log(self.level, line.strip('\n'))
+            print(line)
+
+        self.pipeReader.close()
+
+    def close(self):
+        """Close the write end of the pipe.
+        """
+        os.close(self.fdWrite)
+
+
+# For testing
+if __name__ == "__main__":
+    import sys
+    from azureml.core import Run
+
+    logpipe = AmlLogPipe(logging.INFO, Run.get_context())
+    with subprocess.Popen(['/bin/ls'], stdout=logpipe, stderr=logpipe) as s:
+        logpipe.close()
+
+    sys.exit()
--- code/train.py
+++ code/train.py
@@ -6,7 +6,12 @@
 import click
 import os
 import subprocess
+import re
+import logging
 from threading import Event, Thread
+import shutil
+
+from aml_logging_wrapper import AmlLogPipe
 
 from azureml.core import Run
 
@@ -17,7 +22,8 @@ BOOSTING_CHOICE = click.Choice(['gbdt', 'rf', 'dart', 'goss'])
 TREE_LEARNER = click.Choice(['serial', 'feature', 'data', 'voting'])
 
 # Determine the MPI_RANK and MPI_MODE
-MPI_RANK = os.environ.get('OMPI_COMM_WORLD_RANK', 0)
+# OMPI_COMM_WORLD_RANK environment variable is set by OpenMPI in parallel context.
+MPI_RANK = int(os.environ.get('OMPI_COMM_WORLD_RANK', 0))
 MPI_MODE = True if os.environ.get('OMPI_COMM_WORLD_RANK') else False
 if MPI_MODE:
     print('OpenMPI cluster run detected:')
@@ -29,74 +35,138 @@ if MPI_MODE:
 run = Run.get_context()
 
 
-# NOTE: For any hyperparameter that you would like to tune, it must included
-#       as a command line argument here.
-
-# TODO: Allow for any arbitrary argument to be passed here - and pass that to LightGBM as a command line argument.
-
 # Click is used as an alternative to argparse. It allows the use of
 # Python decorators to pass CLI arguments.
-@click.command("LightGBM Parallel")
-@click.option('--conf_file', type=click.Path(exists=True, dir_okay=False), default=None, required=False, help="Path of the LightGBM config file")
-@click.option('--train_data_directory', type=click.Path(exists=True, file_okay=False, dir_okay=True), help='Path(s) of training data. LightGBM will train from this data')
-@click.option('--valid_data_directory', type=click.Path(exists=True, file_okay=False, dir_okay=True), help='Path(s) of validation data. LightGBM will output metrics from this data')
-@click.option("--task", type=TASK_CHOICES, default=None, help="The LightGBM task type")
-@click.option('--objective', type=OBJECTIVE_CHOICES, default=None, help='The objective of the LightGBM training')
-@click.option('--boosting', type=BOOSTING_CHOICE, default=None, help='The type of boosting to use')
-@click.option('--tree_learner', type=TREE_LEARNER, help='The learner used for LightGBM. For parallel processing, use feature, data, or voting')
-@click.option('--num_iterations', type=str)
-@click.option('--num_threads', type=int, help="The number of threads for LightGBM. Default is the number of threads in OpenMP.")
-@click.option('--num_leaves', type=int, help='The max number of leaves in one tree, Default is 31')
-@click.option('--learning_rate', type=float, help='The shrinkage rate. In dart it also affects on normalization weights of dropped trees')
-def main(train_data_directory, valid_data_directory, conf_file, **lgbm_params):
+@click.command("LightGBM Parallel", context_settings={"ignore_unknown_options": True, 'allow_extra_args': True})
+@click.option('--conf_file', type=click.Path(exists=True, dir_okay=False),
+              default=None, required=False, help="Path of the LightGBM config file")
+@click.option('--train_data', type=click.Path(exists=True, file_okay=True, dir_okay=True),
+              help='Path(s) of training data. LightGBM will train from this data')
+@click.option('--valid_data', type=click.Path(exists=True, file_okay=True, dir_okay=True),
+              help='Path(s) of validation data. LightGBM will output metrics from this data')
+@click.option('--output_model', type=str, default='LightGBM_model.txt',
+              help="The filename where the saved model will be stored.")
+@click.option('--local_listen_port', type=int, default=12400)
+@click.pass_context
+def main(context, train_data, valid_data, conf_file, **kwargs):
     """Run LightGBM in parallel"""
-    # OMPI_COMM_WORLD_RANK environment variable is set by OpenMPI in parallel context.
 
-    _run_lgbm(_prepare_lgbm_command_line(conf_file=conf_file, train_data=train_data_directory,
-                                     validation_data=valid_data_directory, param_dict=lgbm_params))
+    # Parse the unknown and extra known arguments into a single list
+    lgbm_params = {**kwargs, **parse_unknown_args(context.args)}
+
+    lgbm_params['machines'] = create_machine_list(kwargs['local_listen_port'])
+
+    command_line = prepare_lgbm_command_line(conf_file=conf_file, train_data=train_data,
+                                             validation_data=valid_data, param_dict=lgbm_params)
+
+    run_lgbm(command_line)
+
+    if MPI_RANK == 0:
+        capture_model_file(filename=lgbm_params['output_model'])
 
 
-def _parse_files_from_directory(directory):
-    """Take a directory and return a list of files in the directory. LightGBM expects a list of files in a comma seperated string."""
-    files = [entry.path for entry in os.scandir(directory) if entry.is_file()]
-    return ','.join(files)
+def create_machine_list(port: int) -> str:
+    iplist = []
+    for ip in os.environ.get('AZ_BATCH_NODE_LIST').split(';'):
+        iplist.append(f'{ip}:{port}')
+
+    return ','.join(iplist)
 
 
+def parse_unknown_args(arg_list: list) -> dict:
+    """Takes a list of args and creates a dict object.
 
-def _dict_to_param_string(params_dict: dict) -> str:
+    Looks for '--' starting a string and uses that for a 
+
+    Example: 
+
+        parse_unknown_args(['--foo', 'bar', 'baz', '--test', 1])
+
+        returns: {"foo": "bar", "test": 1}
+    """
+
+    param_dict = {}
+    for arg in pairwise(arg_list):
+        if arg[0].startswith('--'):
+            if arg[1].startswith('--'):
+                raise ValueError(f"The arguments passed must be in --arg value format. Argument '{arg[0]}' appears to have no associate value.")
+            param_dict[arg[0].strip('--')] = arg[1]
+
+    return param_dict
+
+
+def pairwise(iterable) -> zip:
+    """s -> (s0,s1), (s1,s2), (s2, s3), ...
+
+    From https://docs.python.org/3/library/itertools.html
+    """
+    from itertools import tee
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def expand_path(path: str) -> str:
+    """Take a path and return a list of files if a directory, otherwise return the path. LightGBM expects a list of files in a comma seperated string."""
+    # Check if it is a directory, if so return all files in the directory
+    if os.path.isdir(path):
+        files = [entry.path for entry in os.scandir(path) if entry.is_file()]
+        return ','.join(files)
+    # Otherwise return the file name
+    else:
+        return path
+
+
+def dict_to_param_list(params_dict: dict) -> list:
     """Take a dict object and convert to a string of key=value seperated by spaces"""
-    param_string = []
+    param_list = []
     for key, value in params_dict.items():
         # Add the parameter to the string if it is not None
         if value:
-            param_string.append(f'{key}={value}')
+            param_list.append(f'{key}={value}')
 
-    return " ".join(param_string)
+    return param_list
 
 
-def _prepare_lgbm_command_line(conf_file: str = None, train_data: str = None, validation_data: str = None, param_dict: dict = None) -> str:
+def prepare_lgbm_command_line(conf_file: str = None,
+                              train_data: str = None,
+                              validation_data: str = None,
+                              param_dict: dict = None) -> list:
     """Prepare the string that will be passed to the shell to start the LGBM run"""
-    command_list = ['lightgbm']
+    command_list = ['/usr/local/bin/lightgbm']
 
     if conf_file:
         command_list.append(f'config={conf_file}')
     if train_data:
-        command_list.append(f'data={_parse_files_from_directory(train_data)}')
+        command_list.append(f'data={expand_path(train_data)}')
     if validation_data:
         command_list.append(
-            f'valid={_parse_files_from_directory(validation_data)}')
+            f'valid={expand_path(validation_data)}')
     if param_dict:
-        command_list.append(_dict_to_param_string(param_dict))
+        command_list += dict_to_param_list(param_dict)
+        # if len(param_list) != 0:
+        #     command_list += param_list
 
-    return " ".join(command_list)
-
-
-def _parse_log_file():
-    pass
+    return command_list
 
 
-def _run_lgbm(command_line: str):
-    subprocess.call(command_line) 
+def run_lgbm(command_line: str):
+    logpipe = AmlLogPipe(logging.INFO, Run.get_context())
+    with subprocess.Popen(command_line, stdout=logpipe, stderr=logpipe) as s:
+        logpipe.close()
+
+    if s.returncode != 0:
+        raise RuntimeError(f'Lightgbm exited with exit code: {s.returncode}')
+
+
+def capture_model_file(filename: str = "LightGBM_model.txt"):
+    """LightGBM will save the model as a text file in the current directory. 
+       By default the name is 'LightGBM_model.txt' 
+
+       This function moves the file to the outputs folder to be captured by AML
+    """
+    os.makedirs('outputs', exist_ok=True)
+    shutil.copy(filename, os.path.join('./outputs', filename))
 
 
 if __name__ == "__main__":
